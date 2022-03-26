ceafd7c65b538dddff69224c50f4c90f52df2059
tlr24 <tlr24@njit.edu>
2022-03-26 18:37:51 -0400

    Updated Calculations history class & tests

---
+++ calculator/history/__init__.py
@@ -0,0 +1,80 @@
+""""Calculation history class"""
+from calculator.calculations import Addition, Subtraction, Multiplication, Division
+
+
+class Calculations:
+    """Calculation history abstract base class"""
+    # pylint: disable=too-few-public-methods
+    history = []
+
+    @staticmethod
+    def add_calculation(calculation):
+        """Add a calculation to the history"""
+        Calculations.history.append(calculation)
+        return True
+
+    @staticmethod
+    def clear_history():
+        """Clear history of calculations"""
+        Calculations.history.clear()
+        return True
+
+    @staticmethod
+    def get_calculation(num):
+        """Get a calculation from the history"""
+        return Calculations.history[num]
+
+    @staticmethod
+    def count_calculations():
+        """Count the number of calculations in history"""
+        return len(Calculations.history)
+
+    @staticmethod
+    def remove_calculation(num):
+        """Remove a specific calculation"""
+        Calculations.history.remove(Calculations.history[num])
+        return True
+
+    @staticmethod
+    def get_calculation_first():
+        """Get the first calculation in history"""
+        return Calculations.history[0]
+
+    @staticmethod
+    def get_calculation_first_result():
+        """Get the result of the first calculation in history"""
+        return Calculations.history[0].get_result()
+
+    @staticmethod
+    def get_calculation_last():
+        """Get the last calculation in history"""
+        return Calculations.history[-1]
+
+    @staticmethod
+    def get_calculation_last_result():
+        """Get the result of the last calculation in history"""
+        return Calculations.history[-1].get_result()
+
+    @staticmethod
+    def add_addition_calculation(values):
+        """Create an addition and add object to history using factory method create"""
+        Calculations.add_calculation(Addition.create(values))
+        return True
+
+    @staticmethod
+    def add_subtraction_calculation(values):
+        """Create a subtraction object to history using factory method create"""
+        Calculations.add_calculation(Subtraction.create(values))
+        return True
+
+    @staticmethod
+    def add_multiplication_calculation(values):
+        """Add a multiplication object to history using factory method create"""
+        Calculations.add_calculation(Multiplication.create(values))
+        return True
+
+    @staticmethod
+    def add_division_calculation(values):
+        """Add a division object to history using factory method create"""
+        Calculations.add_calculation(Division.create(values))
+        return True
---
+++ tests/history_test.py
@@ -0,0 +1,122 @@
+"""Test history calculations"""
+import pytest
+from calculator.history import Calculations
+from calculator.calculations import Addition
+
+
+@pytest.fixture
+def clear_history_fixture():
+    """A fixture that will run each time you pass it to a test to clear history"""
+    # pylint: disable=redefined-outer-name,unused-argument
+    Calculations.clear_history()
+
+
+def test_add_addition_calculation_to_history():
+    """Testing adding a calculation to history"""
+    values = (1, 2)
+    Calculations.add_addition_calculation(values)
+    assert len(Calculations.history) == 1
+
+
+def test_add_subtraction_calculation_to_history():
+    """Testing adding a subtraction calculation to history"""
+    values = (1, 2)
+    Calculations.add_subtraction_calculation(values)
+    assert len(Calculations.history) == 2
+
+
+def test_add_multiplication_calculation_to_history():
+    """Testing adding a subtraction calculation to history"""
+    values = (1, 2)
+    Calculations.add_multiplication_calculation(values)
+    assert len(Calculations.history) == 3
+
+
+def test_add_division_calculation_to_history():
+    """Testing adding a division calculation to history"""
+    values = (1, 2)
+    Calculations.add_division_calculation(values)
+    assert len(Calculations.history) == 4
+
+
+def test_clear_calculations_history():
+    """Testing clearing the calculations history"""
+    values = (1, 2)
+    addition = Addition(values)
+    Calculations.history.append(addition)
+    assert len(Calculations.history) == 5
+    assert Calculations.clear_history() is True
+
+
+def test_get_calculation():
+    """Test getting a calculation from the history"""
+    values = (1, 2)
+    addition = Addition(values)
+    Calculations.history.append(addition)
+    assert Calculations.get_calculation(0).get_result() == 3
+
+
+def test_count_calculations(clear_history_fixture):
+    """Testing getting a count of the number of calculations in calculation history"""
+    # pylint: disable=unused-variable,unused-argument,redefined-outer-name
+    values = (1, 2)
+    for i in range(3):
+        Calculations.history.append(Addition(values))
+    assert Calculations.count_calculations() == 3
+
+
+def test_remove_calculation(clear_history_fixture):
+    """Testing removing a specific calculation"""
+    # pylint: disable=unused-argument,redefined-outer-name
+    numbers = (1, 1)
+    addition = Addition(numbers)
+    Calculations.history.append(addition)
+    assert Calculations.remove_calculation(0) is True
+
+
+def test_get_calculation_last(clear_history_fixture):
+    """Testing getting the last calculation in history"""
+    # pylint: disable=unused-argument,redefined-outer-name
+    numbers = (2, 1)
+    numbers2 = (4, 5)
+    addition = Addition(numbers)
+    addition2 = Addition(numbers2)
+    Calculations.history.append(addition)
+    Calculations.history.append(addition2)
+    assert Calculations.get_calculation_last() == addition2
+
+
+def test_get_calculation_last_result(clear_history_fixture):
+    """Testing getting the result of the last calculation in history"""
+    # pylint: disable=unused-argument,redefined-outer-name
+    numbers = (2, 1)
+    numbers2 = (4, 5)
+    addition = Addition(numbers)
+    addition2 = Addition(numbers2)
+    Calculations.history.append(addition)
+    Calculations.history.append(addition2)
+    assert Calculations.get_calculation_last_result() == 9
+
+
+def test_get_calculation_first(clear_history_fixture):
+    """Testing getting the first calculation in history"""
+    # pylint: disable=unused-argument,redefined-outer-name
+    numbers = (2, 1)
+    numbers2 = (4, 5)
+    addition = Addition(numbers)
+    addition2 = Addition(numbers2)
+    Calculations.history.append(addition)
+    Calculations.history.append(addition2)
+    assert Calculations.get_calculation_first() == addition
+
+
+def test_get_calculation_first_result(clear_history_fixture):
+    """Testing getting the result of the first calculation in history"""
+    # pylint: disable=unused-argument,redefined-outer-name
+    numbers = (2, 1)
+    numbers2 = (4, 5)
+    addition = Addition(numbers)
+    addition2 = Addition(numbers2)
+    Calculations.history.append(addition)
+    Calculations.history.append(addition2)
+    assert Calculations.get_calculation_first_result() == 3
