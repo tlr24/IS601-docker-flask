ff0cee4c3be12343859f6fe41a722bf08bdadb2c
tlr24 <tlr24@njit.edu>
2022-03-26 21:47:40 -0400

    Cut down on repetition/simplified some tests in history_test.py w/ added fixture

--- tests/history_test.py
+++ tests/history_test.py
@@ -16,6 +16,12 @@ def values_tuple():
     return 1, 2
 
 
+@pytest.fixture
+def setup_addition_calculation_fixture():
+    """Set up an addition calculation and add it to the history"""
+    Calculations.add_calculation(Addition(values_tuple()))
+
+
 def test_add_addition_calculation_to_history():
     """Testing adding a calculation to history"""
     Calculations.add_addition_calculation(values_tuple())
@@ -46,12 +52,12 @@ def test_clear_calculations_history():
     Calculations.history.append(addition)
     assert len(Calculations.history) == 5
     assert Calculations.clear_history() is True
+    assert Calculations.count_calculations() == 0
 
 
-def test_get_calculation():
+def test_get_calculation(clear_history_fixture, setup_addition_calculation_fixture):
     """Test getting a calculation from the history"""
-    addition = Addition(values_tuple())
-    Calculations.history.append(addition)
+    # pylint: disable=unused-variable,unused-argument,redefined-outer-name
     assert Calculations.get_calculation(0).get_result() == 3
 
 
@@ -59,61 +65,47 @@ def test_count_calculations(clear_history_fixture):
     """Testing getting a count of the number of calculations in calculation history"""
     # pylint: disable=unused-variable,unused-argument,redefined-outer-name
     for i in range(3):
-        Calculations.history.append(Addition(values_tuple()))
+        Calculations.add_calculation(Addition(values_tuple()))
     assert Calculations.count_calculations() == 3
 
 
-def test_remove_calculation(clear_history_fixture):
+def test_remove_calculation(clear_history_fixture, setup_addition_calculation_fixture):
     """Testing removing a specific calculation"""
     # pylint: disable=unused-argument,redefined-outer-name
-    addition = Addition(values_tuple())
-    Calculations.history.append(addition)
     assert Calculations.remove_calculation(0) is True
 
 
-def test_get_calculation_last(clear_history_fixture):
+def test_get_calculation_last(clear_history_fixture, setup_addition_calculation_fixture):
     """Testing getting the last calculation in history"""
     # pylint: disable=unused-argument,redefined-outer-name
-    numbers = (2, 1)
-    numbers2 = (4, 5)
-    addition = Addition(numbers)
-    addition2 = Addition(numbers2)
+    addition = Addition(values_tuple())
     Calculations.history.append(addition)
-    Calculations.history.append(addition2)
-    assert Calculations.get_calculation_last() == addition2
+    assert Calculations.get_calculation_last() == addition
 
 
-def test_get_calculation_last_result(clear_history_fixture):
+def test_get_calculation_last_result(clear_history_fixture, setup_addition_calculation_fixture):
     """Testing getting the result of the last calculation in history"""
     # pylint: disable=unused-argument,redefined-outer-name
-    numbers = (2, 1)
-    numbers2 = (4, 5)
+    numbers = (4, 5)
     addition = Addition(numbers)
-    addition2 = Addition(numbers2)
     Calculations.history.append(addition)
-    Calculations.history.append(addition2)
     assert Calculations.get_calculation_last_result() == 9
 
 
 def test_get_calculation_first(clear_history_fixture):
     """Testing getting the first calculation in history"""
     # pylint: disable=unused-argument,redefined-outer-name
-    numbers = (2, 1)
-    numbers2 = (4, 5)
-    addition = Addition(numbers)
-    addition2 = Addition(numbers2)
+    addition = Addition(values_tuple())
+    addition2 = Addition(values_tuple())
     Calculations.history.append(addition)
     Calculations.history.append(addition2)
     assert Calculations.get_calculation_first() == addition
 
 
-def test_get_calculation_first_result(clear_history_fixture):
+def test_get_calculation_first_result(clear_history_fixture, setup_addition_calculation_fixture):
     """Testing getting the result of the first calculation in history"""
     # pylint: disable=unused-argument,redefined-outer-name
-    numbers = (2, 1)
-    numbers2 = (4, 5)
+    numbers = (4, 5)
     addition = Addition(numbers)
-    addition2 = Addition(numbers2)
-    Calculations.history.append(addition)
-    Calculations.history.append(addition2)
+    Calculations.add_calculation(addition)
     assert Calculations.get_calculation_first_result() == 3
